b72489b142e71befee566ad8dc6bb627664ba19d
Ahmed <achieboy_97@hotmail.com>
2012-08-27 14:01:22 +0200

    Macha - WebServer
    UPDATED Index file

--- Yes PHP/bin/htdocs/index.php
+++ Yes PHP/bin/htdocs/index.php
@@ -1,6 +1,6 @@
 <center>
-<h1>Simple Web-Server - Ahmed Bakay</h1>
-<?php echo"<h2 style=color:green;>PHP works !</h2><br><br><br>";
+<h1>Macha - WebServer</h1>
+<?php echo"<h2 style=color:green;>PHP works !</h2><br><a href=http://abakay.zxq.net/>Ahmed bakay</a><br><a href=http://abakay.zxq.net/macha>Macha - WebServer</a><br><br><br><br>";
 phpinfo(); 
 ?>
 </center>
