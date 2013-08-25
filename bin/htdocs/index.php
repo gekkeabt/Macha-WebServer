7533a9d90ab4920e08a0882c4caea236ccd9d486
Ahmed <ahmedbakay@outlook.com>
2013-08-25 10:44:45 +0200

    Website update

--- bin/htdocs/index.php
+++ bin/htdocs/index.php
@@ -1,7 +1,7 @@
 <center>
 <h1>Macha - WebServer</h1>
 
-<?php echo"<h2 style=color:green;>PHP works !</h2><br><a href=http://abakay.zxq.net/>Ahmed bakay</a><br><a href=http://abakay.zxq.net/macha>Macha - WebServer</a><br><br><br><br>";
+<?php echo"<h2 style=color:green;>PHP works !</h2><br><a href=http://abakay.tk/>Ahmed bakay</a><br><br><br><br>";
 phpinfo(); 
 ?>
 
